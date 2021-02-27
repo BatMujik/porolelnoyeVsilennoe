dccd2065d55ff5ac3d81514610d0a9b705087d2e
Batman <artyom.hakobyan.fantasy@gmail.com>
2021-02-27 08:13:57 -0800

    initial commit

---
+++ makeHtml.bat
@@ -0,0 +1,16 @@
+md css
+md js
+md img
+echo  ^<!doctype html^> >> index.html
+echo  ^<html lang="en"^> >> index.html
+echo  	^<head^> >> index.html
+echo 		^<meta charset="UTF-8"^> >> index.html
+echo 		^<meta name="viewport" content="initial-scale=1, maximum-scale=1"^> >> index.html
+echo 		^<link rel="stylesheet" href="style.css"^> >> index.html
+echo  		^<title^>^Batman^</title^> >> index.html				
+echo 	^</head^>  >> index.html
+echo  ^<body^>	>> index.html
+echo  ^</body^>	>> index.html
+echo  ^</html^> >> index.html
+echo  >>css\style.css
+echo  >>js\script.js
